77ebb47bbb5076c44c5412f6b623ef8565b3a600
nutchapon hanouypornlert <salaryhero@nutchapons-MacBook-Air.local>
2023-08-14 23:09:08 +0700

    add code

--- src/day1/TwoCrystalBalls.ts
+++ src/day1/TwoCrystalBalls.ts
@@ -1,3 +1,6 @@
 export default function two_crystal_balls(breaks: boolean[]): number {
-
+  const jumpAmount = Math.sqrt(breaks.length);
+  // [n, n2, n3, n4]
+  // jump by square root of n
+  // backward check on step
 }
