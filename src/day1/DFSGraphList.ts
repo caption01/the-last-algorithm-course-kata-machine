b38f3cacb268f5094644710197b6f33ad57f63ce
nutchapon hanouypornlert <salaryhero@nutchapons-MacBook-Air.local>
2023-08-22 08:47:32 +0700

    code

--- src/day1/DFSGraphList.ts
+++ src/day1/DFSGraphList.ts
@@ -1,3 +1,44 @@
-export default function dfs(graph: WeightedAdjacencyList, source: number, needle: number): number[] | null {
+function walk(
+  graph: WeightedAdjacencyList,
+  curr: number,
+  needle: number,
+  seen: boolean[],
+  path: number[],
+): boolean {
+  if (curr === needle) return true;
 
+  if (seen[curr]) return true;
+
+  seen[curr] = true;
+
+  // recurse
+  // pre
+  path.push(curr);
+
+  // recurse
+  const list = graph[curr];
+  for (let i = 0; i < list.length; i++) {
+    const edge = list[i];
+
+    if (walk(graph, edge.to, needle, seen, path)) {
+      path.push(edge.to);
+      return true;
+    }
+  }
+
+  // post
+  path.pop();
+
+  return false;
+}
+
+export default function dfs(
+  graph: WeightedAdjacencyList,
+  source: number,
+  needle: number,
+): number[] | null {
+  const seen = new Array(graph.length).fill(false);
+  const path: number[] = [];
+
+  walk(graph, source, needle, seen, path);
 }
